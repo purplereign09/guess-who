2762f43ae547f32c7454063354f2c0c300fe4bdc
Brad Johansen <brad.d.johansen@gmail.com>
2021-07-26 16:46:21 -0500

    at class end

--- client.js
+++ client.js
@@ -2,13 +2,43 @@ console.log('Here are all the available people:', people);
 
 $(document).ready(onReady);
 
+let randomNum = 0;
+
 function onReady(){
-    console.log(' ready');
-    console.log('test');
+    renderPeople();
+    $(document).on('click', '.person', personClicked);
 }
 
+/**
+ * Render People
+ * Renders people objects to dom
+ */
+function renderPeople() {
+    // DO SOMETHING
+    let index = 0;
+    for (person of people) {
+        console.log(person);
+        $('#people').append(
+            `<div data-person="${person.name}" class="person">
+                <img data-index="${index}" src='${person.img}'></img>
+            </div>`
+        );
+        index++
+    }
+}
 
-
+function personClicked() {
+    let randomNum = randomNum(1, people.length);
+    if(randomNum === $(this).data('index')) {
+        console.log('MATCH');
+    }
+    console.log( 'our random num' );
+    console.log('clicked ', this);
+}
+/**
+ * Rand Number
+ * returns a random number
+ */
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
